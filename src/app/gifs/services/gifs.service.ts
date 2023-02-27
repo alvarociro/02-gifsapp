24aab54ead8eafad2414b3d9356743d44abc5606
Alvaro Ciro <acasaverde13@gmail.com>
2023-02-26 22:47:59 -0500

    20230226

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,12 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _historial: string[] = [];
+  private apiKey: string ='4AOhHZ4zWqRawP0fj8et5i3nbsbnxcjq';
 
-  get historial(){
+  get historial(){    
     return [...this._historial];
   }
 
-  buscarGifs (query: string){
-    this._historial.unshift(query);
+  buscarGifs (query: string=''){
+    query=query.trim().toLowerCase();
+
+    if (query.trim().length===0){
+      return;
+    }
+
+    if (!this._historial.includes(query)){
+      this._historial.unshift(query);
+    }
+
+    this._historial = this._historial.splice(0,10);
+
+    console.log(this._historial);
   }
 }
