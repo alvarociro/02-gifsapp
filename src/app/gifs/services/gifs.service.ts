818c0d8bfc30aba4d579c5163e637b40e6954efe
Alvaro Ciro <acasaverde13@gmail.com>
2023-03-03 09:05:35 -0500

    20230303

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,9 +8,14 @@ export class GifsService {
   private _historial: string[] = [];
   private apiKey: string ='4AOhHZ4zWqRawP0fj8et5i3nbsbnxcjq';
 
+  //TODO: CANBIAR ANY POR SU TIPO CORRESPONDIOENTE
+  public resultados: any[] = [];
+
   get historial(){    
     return [...this._historial];
   }
+
+  constructor ( private http:HttpClient){}
 
   buscarGifs (query: string=''){
     query=query.trim().toLowerCase();
@@ -20,10 +26,14 @@ export class GifsService {
 
     if (!this._historial.includes(query)){
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
     }
 
-    this._historial = this._historial.splice(0,10);
 
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=4AOhHZ4zWqRawP0fj8et5i3nbsbnxcjq&q=${query}&limit=10`)
+      .subscribe((resp:any) =>{
+        console.log(resp);
+        this.resultados=resp.data;
+      });
   }
 }
