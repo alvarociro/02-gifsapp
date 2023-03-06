2bcba9fb9d3177fc3a5073c91602df19abc3db82
Alvaro Ciro <acasaverde13@gmail.com>
2023-03-05 21:53:44 -0500

    fin proyecto Gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private _historial: string[] = [];
+  private servicioUrl:string ='https://api.giphy.com/v1/gifs';
   private apiKey: string ='4AOhHZ4zWqRawP0fj8et5i3nbsbnxcjq';
 
   //TODO: CANBIAR ANY POR SU TIPO CORRESPONDIOENTE
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(){    
     return [...this._historial];
   }
 
-  constructor ( private http:HttpClient){}
+  constructor ( private http:HttpClient){
+    this._historial =JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados =JSON.parse(localStorage.getItem('resultados')!) || [];
+/*     if (localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+    } */
+  }
 
   buscarGifs (query: string=''){
     query=query.trim().toLowerCase();
@@ -27,13 +35,19 @@ export class GifsService {
     if (!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+
     }
 
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit','10')
+      .set('q',query);
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=4AOhHZ4zWqRawP0fj8et5i3nbsbnxcjq&q=${query}&limit=10`)
-      .subscribe((resp:any) =>{
-        console.log(resp);
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
+      .subscribe((resp) =>{
         this.resultados=resp.data;
+        localStorage.setItem('resultados',JSON.stringify(this.resultados));
       });
   }
 }
